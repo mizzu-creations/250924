c1554c9c0a6c4b61498a3b7af32ac335dd8ca53a
mizzu-creations <mizzu.creations@gmail.com>
2025-09-24 23:49:52 +0900

    기본 세팅 및 마크업

---
+++ 3d-asset/main.js
@@ -0,0 +1,8 @@
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
+import Lenis from "lenis";
+
+document.addEventListener("DOMContentLoaded", () => {});
